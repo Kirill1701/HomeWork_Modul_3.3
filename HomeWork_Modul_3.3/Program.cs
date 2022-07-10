275f4a743ed0366d5b530c5a8392f7982970320a
Кирилл <@HOME-PC>
2022-07-10 11:19:52 +1000

    Добавьте файлы проекта.

---
+++ HomeWork_Modul_3.3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HomeWork_Modul_3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число\n");
+            int n = int.Parse(Console.ReadLine());
+            bool number = false;
+
+            while (n >= 1)
+            {
+                if (n % 2 == 0)
+                {
+                    Console.Write(true);
+
+                }
+                else
+                {
+                    Console.Write(number);
+                }
+                break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
